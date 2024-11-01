7e577c6690af4f1955027c346760e08692fd6079
aoitchan0212 <aoi.t.chan0212@gmail.com>
2024-11-01 14:48:51 +0900

    ボタン１０個

--- CS_form/CS_form/TestButton.cs
+++ CS_form/CS_form/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,19 @@ namespace CS_form
 {
     class TestButton : Button
     {
-        public TestButton()
+        public TestButton(int x,int y,int widht,int height)
         {
+            //ClickイベントにOnClick関数を問登録
+            //botannwo 
+            
             Click += OnClick;
+
+            //ボタン内に文字を表示
+            Text = "ボタン";
+
+            Location = new Point(x,y);         //ボタンの位置
+            Size = new Size(widht,height);     //ボタンのサイズ
+
         }
 
         public void OnClick(object sender, EventArgs s)
