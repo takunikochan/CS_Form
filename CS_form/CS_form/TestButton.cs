86cac7aa627292e90516a1153f2b0481bfdf79d1
aoitchan0212 <aoi.t.chan0212@gmail.com>
2024-11-06 12:10:28 +0900

    ボタンを任意の文字列に変更

--- CS_form/CS_form/TestButton.cs
+++ CS_form/CS_form/TestButton.cs
@@ -13,7 +13,7 @@ namespace CS_form
     {
         Form1 _form1;
 
-        public TestButton(Form1 form1, int id,int x,int y,int widht,int height)
+        public TestButton(Form1 form1, int id, int x, int y, int widht, int height)
         {
             _form1 = form1;
 
@@ -23,15 +23,56 @@ namespace CS_form
             Click += OnClick;
 
             //ボタン内に文字を表示
-           Text = id.ToString();
-           
-            //ボタンの生成場所を指定
-            Location = new Point(x,y);        
-            
-            //ボタンのサイズを指定
-            Size = new Size(widht,height);    
+            Text = id.ToString();
+            if (id == 0)
+            {
+                Text = "あ!";
+            }
+            else if (id == 1)
+            {
+                Text = "い!";
+            }
+            else if (id == 2)
+            {
+                Text = "う!";
+            }
+            else if (id == 3)
+            {
+                Text = "え!";
+            }
+            else if (id == 4)
+            {
+                Text = "お!";
+            }
+            else if (id == 5)
+            {
+                Text = "か!";
+            }
+            else if (id == 6)
+            {
+                Text = "き!";
+            }
+            else if (id == 7)
+            {
+                Text = "く!";
+            }
+            else if (id == 8)
+            {
+                Text = "け!";
+            }
+            else if (id == 9)
+            {
+                Text = "こ!";
+            }
 
-        }
+            //ボタンの生成場所を指定
+            Location = new Point(x, y);
+
+            //ボタンのサイズを指定
+            Size = new Size(widht, height);
+
+            }
+        
 
         public void OnClick(object sender, EventArgs s)
         {
